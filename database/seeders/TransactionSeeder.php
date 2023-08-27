580532dd2fb761f1c8d0b862aa99e5df28d83c50
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2023-08-27 20:53:11 +0700

    add new seeder

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -16,21 +16,39 @@ class TransactionSeeder extends Seeder
         Transaction::create([
             'product_id' => 1,
             'date' => '2019-08-27',
-            'qty' => 2,
+            'qty' => 52,
             'total' => 20000
         ]);
 
         Transaction::create([
             'product_id' => 2,
             'date' => '2020-08-27',
-            'qty' => 1,
+            'qty' => 46,
             'total' => 10000
         ]);
 
         Transaction::create([
             'product_id' => 3,
             'date' => '2021-08-27',
-            'qty' => 3,
+            'qty' => 60,
+            'total' => 30000
+        ]);
+        Transaction::create([
+            'product_id' => 3,
+            'date' => '2022-08-27',
+            'qty' => 64,
+            'total' => 30000
+        ]);
+        Transaction::create([
+            'product_id' => 3,
+            'date' => '2023-08-27',
+            'qty' => 72,
+            'total' => 30000
+        ]);
+        Transaction::create([
+            'product_id' => 3,
+            'date' => '2018-08-27',
+            'qty' => 64,
             'total' => 30000
         ]);
     }
